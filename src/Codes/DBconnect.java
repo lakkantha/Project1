e47a25ddc4c0deb2a72ecb45181eb34fc263b28f
Lakkantha <Lakkantha@DESKTOP-HT3KA7J>
2020-07-21 13:44:01 +0530

    create db connection

---
+++ src/Codes/DBconnect.java
@@ -0,0 +1,25 @@
+package Codes;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Lakkantha
+ */
+
+public class DBconnect {
+    
+    public static Connection connect(){
+        
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3308/project1","root","");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return conn;
+        
+        
+    }
+    
+}
